ab2ddeec8b9a7f2e12344baf02c0ddf8b7cb9b4a
lamiqv <laumiqv@gmail.com>
2020-07-10 14:44:08 +0300

    add overlay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
+import {
+  Router,
+  Event as RouterEvent,
+  NavigationStart,
+  NavigationEnd,
+  NavigationCancel,
+  NavigationError
+} from '@angular/router';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,13 +17,38 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class AppComponent implements OnInit {
 
-  constructor(private translateService: TranslateService) {
+  public showOverlay = true;
+
+  constructor(private translateService: TranslateService, private router: Router) {
+
+    router.events.subscribe((event: RouterEvent) => {
+      this.navigationInterceptor(event)
+    })
 
     const defaultLng = (navigator.language || navigator['browserLanguage']).split('-')[0];
     const langToSet = window['jQuery']['cookie']('lng') || (['deu', 'eng', 'vie', 'ita'].includes(defaultLng) ? defaultLng : 'eng');
 
     this.translateService.use(langToSet);
+
   }
+
+  navigationInterceptor(event: RouterEvent): void {
+    if (event instanceof NavigationStart) {
+      this.showOverlay = true;
+    }
+    if (event instanceof NavigationEnd) {
+      this.showOverlay = false;
+    }
+
+    // Set loading state to false in both of the below events to hide the spinner in case a request fails
+    if (event instanceof NavigationCancel) {
+      this.showOverlay = false;
+    }
+    if (event instanceof NavigationError) {
+      this.showOverlay = false;
+    }
+  }
+
 
   onActivate(event) {
     window.scroll(0, 0);
