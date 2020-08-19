5d3657b1d33fbc40d727240735dba84919a2157f
Amary <laumiqv@gmail.com>
2020-08-19 14:21:49 +0300

    upd translate for about duc - milestones

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  public milestinesData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
   constructor() { }
 
   ngOnInit() {
