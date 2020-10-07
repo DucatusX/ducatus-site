49f22d4d88ef21175d939815934853effb3094b6
Gleb Chistyakov <gleb@MacBook-Pro-Gleb.local>
2020-10-07 16:47:04 +0300

    delete duc address input

--- src/app/service/buy/buy.service.ts
+++ src/app/service/buy/buy.service.ts
@@ -2,10 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpService } from '../http/http.service';
 
 @Injectable({ providedIn: 'root' })
-
 export class BuyService {
-
-  constructor(private httpService: HttpService) { }
+  constructor(private httpService: HttpService) {}
 
   public getRates() {
     return this.httpService.get(`rates/`).toPromise();
@@ -19,23 +17,30 @@ export class BuyService {
     return this.httpService.get(`get_lotteries_info/`).toPromise();
   }
 
-  public getLotteryPlayers() {
-    return this.httpService.get(`lotteries_players/`).toPromise();
+  public getLotteryPlayers(loteryId, page) {
+    return this.httpService
+      .get(`lotteries_players/?lottery_id=${loteryId}&page=${page}`)
+      .toPromise();
   }
 
   public getValidateDucatusAddress(address: string) {
-    return this.httpService.post(`validate_ducatus_address/`, { address }).toPromise();
+    return this.httpService
+      .post(`validate_ducatus_address/`, { address })
+      .toPromise();
   }
 
-  public getCardLink(amount: number, currency: string, duc_address: string, email: string) {
-    return this.httpService.post(`add_charge/`, { amount, currency, duc_address, email }).toPromise();
+  public getCardLink(amount: number, currency: string, email: string) {
+    return this.httpService
+      .post(`add_charge/`, { amount, currency, email })
+      .toPromise();
   }
 
-  public getExchange(address: string, currency: string, toEmail: string) {
-    return this.httpService.post(`exchange/`, {
-      to_address: address,
-      to_currency: currency,
-      email: toEmail
-    }).toPromise();
+  public getExchange(currency: string, toEmail: string) {
+    return this.httpService
+      .post(`exchange/`, {
+        to_currency: currency,
+        email: toEmail,
+      })
+      .toPromise();
   }
 }
