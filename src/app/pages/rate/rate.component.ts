215c8b935af2bc7bde8ac89edc2a8e6132689122
Gleb Chistyakov <gleb4ist@gmail.com>
2021-03-09 14:47:10 +0300

    finish duc-376

--- src/app/pages/rate/rate.component.ts
+++ src/app/pages/rate/rate.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { RateService } from '../../service/rate/rate.service';
+import { BigNumber } from 'bignumber.js';
 
 @Component({
   selector: 'app-rate',
@@ -8,12 +10,35 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class RateComponent {
   public rateGroup: FormGroup;
-  constructor(private formBuilder: FormBuilder) {
-    this.rateGroup = this.formBuilder.group({
-      duc: [5490, Validators.compose([Validators.required])],
-      ducx: [549, Validators.compose([Validators.required])],
-    });
+  private ratio: string;
+  constructor(private formBuilder: FormBuilder, private rateService: RateService) {
+    this.rateService
+      .getRates()
+      .then((res: any) => {
+        this.rateGroup = this.formBuilder.group({
+          duc: [res.DUC.USD, Validators.compose([Validators.required])],
+          ducx: [res.DUCX.USD, Validators.compose([Validators.required])],
+        });
+        this.ratio = new BigNumber(res.DUCX.USD).dividedBy(res.DUC.USD).toString();
+      })
+      .catch((res) => console.log(res, 'get rates'));
   }
 
-  public setRates(): void {}
+  changeAmount(currency): void {
+    if (currency === 'duc') {
+      this.rateGroup.value.ducx = new BigNumber(this.rateGroup.value.duc).multipliedBy(this.ratio).toString();
+    } else {
+      this.rateGroup.value.duc = new BigNumber(this.rateGroup.value.ducx).dividedBy(this.ratio).toString();
+    }
+  }
+
+  public setRates(): void {
+    this.rateService
+      .changeRates({
+        DUC: +this.rateGroup.value.duc,
+        DUCX: +this.rateGroup.value.ducx,
+      })
+      .then((res) => console.log(res, 'changeRates'))
+      .catch((res) => console.log(res, 'changeRates'));
+  }
 }
